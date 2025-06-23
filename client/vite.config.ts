96d8ac6c84b2422a3d28334c936e6a281401f339
LucS0MA <lucien.kohli@gmail.com>
2025-06-23 23:43:47 +0200

    dockerized client server & nginx

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,9 +5,10 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
+  server: { host: true, hmr: { path: "/hmr" }, allowedHosts: true },
+    resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
